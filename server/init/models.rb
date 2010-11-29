46e19b5eff20d506090da124c48170c27c3392d2
Nicolás Hock Isaza <nhocki@gmail.com>
2010-11-29 03:32:28 -0500

    Added ajax create to centres index

--- server/init/models.rb
+++ server/init/models.rb
@@ -1,5 +1,6 @@
 @config = YAML.load_file("config/database.yml")
 puts "Trying to connect to database #{@config["database"]}"
+ActiveRecord::Base.include_root_in_json = true
 ActiveRecord::Base.establish_connection(:adapter => @config["adapter"], :host => @config["host"], :database => @config["database"])
 puts "Connection succeeded"
 Dir["models/*.rb"].each do |file| 
--- server/public/scripts/application.js
+++ server/public/scripts/application.js
@@ -5,11 +5,36 @@ jQuery(function($){
     $.post($link.attr("href")+".json", {_method: "DELETE", id: obj_id, format: "json"}, function(data){
       if(data["result"] == "ok"){
         $("#alert").addClass('success').removeClass('failed').html("Resultado exitoso").slideDown('fast').delay(4000).fadeOut(1000);
-        $link.parent().fadeOut("fast");
+        $link.parents(".centre").fadeOut("fast");
+      }else{
+        $("#alert").html.removeClass('success').addClass('failed').("Falló la operación. Intenta de nuevo").slideDown('fast').delay(4000).fadeOut(1000);
+      }
+    })
+    return false;
+  });
+  
+  
+  $("#add_centre").live('submit', function(event){
+    $form = $(this);
+    $.post($form.attr("action")+".json", $form.serialize(), function(data){
+      if(!isNaN(data["centre"]["id"])){
+        $("#alert").addClass('success').removeClass('failed').html("Resultado exitoso").slideDown('fast').delay(4000).fadeOut(1000);
+        var ht = '<div class="centre">';
+      		  ht += '<h4>'+data["centre"]["name"]+'</h4>';
+      		  ht += '<p>';
+      		  ht += '  <a href="/centres/'+data["centre"]["id"]+'">Centre</a>';
+      		  ht += '  <a href="/centres/'+data["centre"]["id"]+'/edit">Edit</a>';
+      		  ht += '  <a href="/centres/'+data["centre"]["id"]+'" class="destroy-link">Destroy</a>';
+      		  ht += '</p>';
+      	    ht += '</div>';
+        $("#centres").append(ht);
+        $("#new-centre-name").val("");
       }else{
         $("#alert").html.removeClass('success').addClass('failed').("Falló la operación. Intenta de nuevo").slideDown('fast').delay(4000).fadeOut(1000);
       }
     })
     return false;
   })
+  
+  
 });
